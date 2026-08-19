593380cd896fbf183296187906327b93968d3b23
N1ckk0 <nick.opuko@gmail.com>
2026-08-19 17:28:47 +0200

    added: handler placeholder

---
+++ backend/internal/handler/event_handler.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	//"backend/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+// EVENTHUB-75: Veranstaltung anlegen
+func CreateEventHandler(c *gin.Context) {
+
+}
+
+// EVENTHUB-77: Veranstaltung als Entwurf speichern
+func SaveEventAsDraftHandler(c *gin.Context) {
+
+}
+
+// EVENTHUB-78: Veranstaltung bearbeiten
+func UpdateEventHandler(c *gin.Context) {
+}
+
+// EVENTHUB-76: Veranstaltung veröffentlichen
+func PublishEventHandler(c *gin.Context) {
+
+}
+
+// EVENTHUB-82: Veranstaltung zurückziehen
+func WithdrawEventHandler(c *gin.Context) {
+
+}
+
+// EVENTHUB-79: Eigene Veranstaltungen anzeigen
+func ListOwnEventsHandler(c *gin.Context) {
+
+}
+
+// EVENTHUB-80: Verkaufte Tickets pro Veranstaltung anzeigen
+func GetSoldTicketsHandler(c *gin.Context) {
+}
+
+// EVENTHUB-81: Freie Plätze pro Veranstaltung anzeigen
+func GetAvailableSeatsHandler(c *gin.Context) {
+
+}
