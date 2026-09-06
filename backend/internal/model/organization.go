65cadb020fbca0438355c8e10fe4ad03e73ce471
X-Core0 <christmann.tameo@gmail.com>
2026-09-06 17:33:56 +0200

    feat(api): added functional data into the api org creation endpoint

--- backend/internal/model/organization.go
+++ backend/internal/model/organization.go
@@ -1,9 +1,16 @@
 package model
 
 type CreateOrganizationRequest struct {
-	InternalName string `json:"name" binding:"required"`
-	DisplayName  string `json:"alias" binding:"required"`
-	OrgAdmin     string `json:"orgAdmin" binding:"required"`
+	InternalName       string  `json:"name" binding:"required"`
+	DisplayName        string  `json:"alias" binding:"required"`
+	OrgAdmin           string  `json:"orgAdmin" binding:"required"`
+	ContactEmail       *string `json:"contactEmail,omitempty"`
+	ContactPhoneNumber *string `json:"contactPhoneNumber,omitempty"`
+	Street             *string `json:"street,omitempty"`
+	HouseNumber        *string `json:"houseNumber,omitempty"`
+	PostalCode         *string `json:"postalCode,omitempty"`
+	City               *string `json:"city,omitempty"`
+	CountryCode        *string `json:"countryCode,omitempty"`
 }
 
 type CreateOrganizationResponse struct {
